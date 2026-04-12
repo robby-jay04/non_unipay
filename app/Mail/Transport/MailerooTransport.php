a3a44794e44acb19665d4641f8d04e00ce1284b9
robby-jay04 <robbyjayibale2@gmail.com>
2026-04-13 01:30:40 +0800

    Mail

--- app/Mail/Transport/MailerooTransport.php
+++ app/Mail/Transport/MailerooTransport.php
@@ -21,19 +21,18 @@ class MailerooTransport extends AbstractTransport
     $fromAddress = $email->getFrom()[0];
     $toAddresses = array_map(fn($a) => $a->getAddress(), $email->getTo());
 
-    $response = Http::withHeaders([
-        'X-API-Key' => $this->apiKey,
-        'Content-Type' => 'application/json',
-    ])->post('https://smtp.maileroo.com/api/v2/send', [
-        'from'         => $fromAddress->getName() 
-                            ? $fromAddress->getName() . ' <' . $fromAddress->getAddress() . '>'
-                            : $fromAddress->getAddress(),
-        'to'           => implode(',', $toAddresses),
-        'subject'      => $email->getSubject(),
-        'html_body'    => $email->getHtmlBody(),
-        'plain_body'   => $email->getTextBody() ?? strip_tags($email->getHtmlBody()),
-    ]);
-
+   $response = Http::withHeaders([
+    'X-Api-Key' => $this->apiKey,
+    'Content-Type' => 'application/json',
+])->post('https://smtp.maileroo.com/api/v2/emails', [
+    'from'         => $fromAddress->getName() 
+                        ? $fromAddress->getName() . ' <' . $fromAddress->getAddress() . '>'
+                        : $fromAddress->getAddress(),
+    'to'           => implode(',', $toAddresses),
+    'subject'      => $email->getSubject(),
+    'html_body'    => $email->getHtmlBody(),
+    'plain_body'   => $email->getTextBody() ?? strip_tags($email->getHtmlBody()),
+]);
     if (!$response->successful()) {
         throw new \Exception('Maileroo API error: ' . $response->body());
     }

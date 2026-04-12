ec7fd92191b3aafbf0c6cbbeebfc29e09bc8adb9
robby-jay04 <robbyjayibale2@gmail.com>
2026-04-12 21:09:02 +0800

    Add resend mailer to config

--- config/mail.php
+++ config/mail.php
@@ -45,6 +45,9 @@ return [
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
+         'resend' => [
+        'transport' => 'resend',
+    ],
 
         'ses' => [
             'transport' => 'ses',
